4ddf7400e8e92a44522ffb3ef1c9ce77287bad59
Alucard24 <dmbaoh2@gmail.com>
2014-03-04 15:33:47 +0100

    Revert "mm/slob: Use free_page instead of put_page for page-size kmalloc allocations"
    This reverts commit 1e7c995deed04e552bcb492c7d64bf5328f8f691.
    
    Signed-off-by: Alucard24 <dmbaoh2@gmail.com>

--- mm/slob.c
+++ mm/slob.c
@@ -500,7 +500,7 @@ void kfree(const void *block)
 		unsigned int *m = (unsigned int *)(block - align);
 		slob_free(m, *m + align);
 	} else
-		__free_pages(sp, compound_order(sp));
+		put_page(sp);
 }
 EXPORT_SYMBOL(kfree);
 
