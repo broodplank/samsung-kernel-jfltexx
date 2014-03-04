4320833a8c857f43311842b340b3d42b3495199c
Alucard24 <dmbaoh2@gmail.com>
2014-03-04 15:34:47 +0100

    Revert "slab: Use page struct fields instead of casting"
    This reverts commit 3eb8371098e8d157c849c7252227f087ed889390.
    
    Signed-off-by: Alucard24 <dmbaoh2@gmail.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -119,10 +119,6 @@ struct page {
 		};
 
 		struct list_head list;	/* slobs list of pages */
-		struct {		/* slab fields */
-			struct kmem_cache *slab_cache;
-			struct slab *slab_page;
-		};
 	};
 
 	/* Remainder is not double word aligned */
