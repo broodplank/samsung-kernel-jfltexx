7904e0158d49abd37235d0268f14c1086022fe15
Alucard24 <dmbaoh2@gmail.com>
2014-03-25 16:34:17 +0100

    Imported and adjusted cpufreq get_cpu_idle_time from 3.14
    Conflicts:
    	drivers/cpufreq/cpufreq_intelliactive.c

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -35,6 +35,7 @@
 int cpufreq_register_notifier(struct notifier_block *nb, unsigned int list);
 int cpufreq_unregister_notifier(struct notifier_block *nb, unsigned int list);
 extern void disable_cpufreq(void);
+cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall, int io_busy);
 #else		/* CONFIG_CPU_FREQ */
 static inline int cpufreq_register_notifier(struct notifier_block *nb,
 						unsigned int list)
