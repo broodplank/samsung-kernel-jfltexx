04affaf3e9aefe7358cd3c128ea077d29073e4dd
Alucard24 <dmbaoh2@gmail.com>
2014-04-13 13:52:35 +0200

    Updated cpu && smp part

--- arch/arm/include/asm/mutex.h
+++ arch/arm/include/asm/mutex.h
@@ -1,12 +1,13 @@
 /*
-* arch/arm/include/asm/mutex.h
-*
-* ARM optimized mutex locking primitives
-*
-* Please look into asm-generic/mutex-xchg.h for a formal definition.
-*/
+ * arch/arm/include/asm/mutex.h
+ *
+ * ARM optimized mutex locking primitives
+ *
+ * Please look into asm-generic/mutex-xchg.h for a formal definition.
+ */
 #ifndef _ASM_MUTEX_H
 #define _ASM_MUTEX_H
+
 /*
  * On pre-ARMv6 hardware this results in a swp-based implementation,
  * which is the most efficient. For ARMv6+, we have exclusive memory
