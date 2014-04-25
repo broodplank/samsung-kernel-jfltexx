f01d0d3f3eb3155aa3f70c150e63e2188b52bf42
Alucard24 <dmbaoh2@gmail.com>
2014-04-25 22:22:08 +0200

    Revert "Replace the offending argument earlier in the boot process by FennyFatal."
    This reverts commit cde2eacfc752331b668a3e74544c78a0fe200407.

--- init/main.c
+++ init/main.c
@@ -68,7 +68,6 @@
 #include <linux/shmem_fs.h>
 #include <linux/slab.h>
 #include <linux/perf_event.h>
-#include <s_funcs.h>
 #include <linux/random.h>
 
 #include <asm/io.h>
@@ -527,7 +526,6 @@ asmlinkage void __init start_kernel(void)
 	setup_arch(&command_line);
 	mm_init_owner(&init_mm, &init_task);
 	mm_init_cpumask(&init_mm);
-	replace_str((char*)&boot_command_line,"androidboot.bootchg=true","androidboot.mode=charger");
 	setup_command_line(command_line);
 	setup_nr_cpu_ids();
 	setup_per_cpu_areas();
