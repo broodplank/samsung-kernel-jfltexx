5e49387ca3a8eb2f5da0b6319759f915ee60b0ff
Alucard24 <dmbaoh2@gmail.com>
2014-05-15 22:21:10 +0200

    cpu_boost: Adjusted input_boost_freq

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -49,7 +49,7 @@ module_param(boost_ms, uint, 0644);
 static unsigned int sync_threshold;
 module_param(sync_threshold, uint, 0644);
 
-static unsigned int input_boost_freq = 1458400;
+static unsigned int input_boost_freq = 1242000;
 module_param(input_boost_freq, uint, 0644);
 
 static unsigned int input_boost_ms = 40;
