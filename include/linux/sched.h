29c9e77efbee5d8bbfe7fe28f35b52ec4625ca32
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2014-05-28 13:30:26 -0700

    sched/fair: Introduce scheduler boost for low latency workloads
    Certain low latency bursty workloads require immediate use of highest
    capacity CPUs in HMP systems. Existing load tracking mechanisms may be
    unable to respond to the sudden surge in the system load within the
    latency requirements. Introduce the scheduler boost feature for such
    workloads. While boost is in effect the scheduler bypasses regular load
    based task placement and prefers highest capacity CPUs in the system
    for all non-small fair sched class tasks. Provide both a kernel and
    userspace API for software that may have apriori knowledge about the
    system workload.
    
    Change-Id: I783f585d1f8c97219e629d9c54f712318821922f
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	include/linux/sched.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1952,14 +1952,6 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p,
 }
 #endif
 
-static inline void set_wake_up_idle(bool enabled)
-{
-	if (enabled)
-		current->flags |= PF_WAKE_UP_IDLE;
-	else
-		current->flags &= ~PF_WAKE_UP_IDLE;
-}
-
 #ifdef CONFIG_NO_HZ
 void calc_load_enter_idle(void);
 void calc_load_exit_idle(void);
@@ -1967,6 +1959,23 @@ void calc_load_exit_idle(void);
 static inline void calc_load_enter_idle(void) { }
 static inline void calc_load_exit_idle(void) { }
 #endif /* CONFIG_NO_HZ */
+
+#ifdef CONFIG_SCHED_HMP
+extern int sched_set_boost(int enable);
+#else
+static inline int sched_set_boost(int enable)
+{
+	return -EINVAL;
+}
+#endif
+
+static inline void set_wake_up_idle(bool enabled)
+{
+	if (enabled)
+		current->flags |= PF_WAKE_UP_IDLE;
+	else
+		current->flags &= ~PF_WAKE_UP_IDLE;
+}
 
 #ifndef CONFIG_CPUMASK_OFFSTACK
 static inline int set_cpus_allowed(struct task_struct *p, cpumask_t new_mask)
