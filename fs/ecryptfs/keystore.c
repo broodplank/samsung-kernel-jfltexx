9069b9ac0f1437b0b7da4bd0e5af10d5aa38d1c3
Alucard24 <dmbaoh2@gmail.com>
2014-06-08 13:32:21 +0200

    Linux 3.4.92
    Conflicts:
    	drivers/char/random.c
    	drivers/staging/zram/zram_drv.c
    	drivers/staging/zram/zram_sysfs.c
    	fs/ecryptfs/keystore.c
    	kernel/futex.c

--- fs/ecryptfs/keystore.c
+++ fs/ecryptfs/keystore.c
@@ -1148,9 +1148,9 @@ decrypt_pki_encrypted_session_key(struct ecryptfs_auth_tok *auth_tok,
 	u8 cipher_code = 0;
 	struct ecryptfs_msg_ctx *msg_ctx;
 	struct ecryptfs_message *msg = NULL;
-	char *auth_tok_sig = NULL;
+	char *auth_tok_sig;
 	char *payload = NULL;
-	size_t payload_len = 0;
+	size_t payload_len;
 	int rc;
 
 	rc = ecryptfs_get_auth_tok_sig(&auth_tok_sig, auth_tok);
